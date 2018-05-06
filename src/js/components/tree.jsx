d477ec2154313a24d0edcf815d180540be129a6f
Tal Bereznitskey <berzniz@gmail.com>
2018-05-06 22:49:48 +0300

    Add hide button

--- src/js/components/tree.jsx
+++ src/js/components/tree.jsx
@@ -1,14 +1,42 @@
 import React from 'react'
 import Branch from './branch.jsx'
 
-export default ({ root }) => {
-  return (
-    <div>
-      {root.list.map(node => (
-        <span key={node.nodeLabel}>
-          <Branch {...node} />
-        </span>
-      ))}
-    </div>
-  )
+class Tree extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.onClose = this.onClose.bind(this)
+
+    this.state = {
+      show: true
+    }
+  }
+
+  onClose () {
+    const show = false
+    this.setState({ show })
+    document.body.classList.toggle('enable_better_github_pr', show)
+  }
+
+  render () {
+    const { root } = this.props
+    const { show } = this.state
+
+    if (!show) {
+      return null
+    }
+
+    return (
+      <div>
+        <button onClick={this.onClose} className='close_button'>✖</button>
+        {root.list.map(node => (
+          <span key={node.nodeLabel}>
+            <Branch {...node} />
+          </span>
+        ))}
+      </div>
+    )
+  }
 }
+
+export default Tree
