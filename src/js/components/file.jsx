4eca87e7b6bd5db4422934227d59fca30b68d038
Tal Bereznitskey <berzniz@gmail.com>
2018-05-11 16:38:45 +0300

    Add Scroll Spy to highlight current visible file

--- src/js/components/file.jsx
+++ src/js/components/file.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import fileIcons from 'file-icons-js'
 
-const File = ({ name, href, hasComments }) => {
+const highlightColor = '#ebebeb'
+const transparentColor = 'transparent'
+
+const File = ({ name, href, hasComments, isVisible }) => {
   const className = fileIcons.getClassWithColor(name)
   return (
-    <div className='github-pr-file'>
+    <div className='github-pr-file' style={{ background: isVisible ? highlightColor : transparentColor }}>
       <span className={`icon ${className}`} />
       <a href={href} className='link-gray-dark'>{name}</a> {hasComments ? ' 💬' : ''}
     </div>
