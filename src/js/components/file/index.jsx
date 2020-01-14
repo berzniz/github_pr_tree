f12ac7a6412bc17664a07a0f9be8b7ea1d9dd49c
Sunny Mistry <sunnyvmistry@gmail.com>
2020-01-13 22:29:34 -0500

    Add Checkmark Next to Viewed File Name

--- src/js/components/file/index.jsx
+++ src/js/components/file/index.jsx
@@ -60,6 +60,7 @@ class File extends React.Component {
             <path fillRule='evenodd' d='M14 1H2c-.55 0-1 .45-1 1v8c0 .55.45 1 1 1h2v3.5L7.5 11H14c.55 0 1-.45 1-1V2c0-.55-.45-1-1-1zm0 9H7l-2 2v-2H2V2h12v8z' />
           </svg>
         ) : null}
+        { isViewed && <span className='github-pr-file-checkmark'>✅</span> }
       </div>
     )
   }
