a4fc0c7dc9afb3ab7a33b22a03f3b2c91c7a9d2d
Tal Bereznitskey <berzniz@gmail.com>
2020-01-15 14:05:52 +0200

    Update build script

--- utils/build.js
+++ utils/build.js
@@ -2,16 +2,32 @@ const chromeWebStoreItemProperty = require('chrome-web-store-item-property')
 const webpack = require('webpack')
 const config = require('../webpack.config')
 const chromeStoreId = 'nfhdjopbhlggibjlimhdbogflgmbiahc'
+
 const bumpMinorVersion = (version) => {
   const splitVersion = version.split('.')
   splitVersion[2] = (1 + parseInt(splitVersion[2], 10))
   return splitVersion.join('.')
 }
 
-console.log('📞  Getting version from Chrome Webstore...')
-chromeWebStoreItemProperty(chromeStoreId).then(value => {
+const getChromeVersion = async () => {
+  console.log('📞  Getting version from Chrome Webstore...')
+  const value = await chromeWebStoreItemProperty(chromeStoreId)
+
   console.log('🔥  Chrome Webstore version:', value.version)
-  const newVersion = bumpMinorVersion(value.version)
+  return value.version
+}
+
+const getNewVersion = async () => {
+  if (process.env.NEW_VERSION) {
+    return process.env.NEW_VERSION
+  }
+
+  const currentChromeVersion = await getChromeVersion()
+  return bumpMinorVersion(currentChromeVersion)
+}
+
+const start = async () => {
+  const newVersion = await getNewVersion()
   console.log('🚀  New version:', newVersion)
   process.env.extension_version = newVersion
   require('./prepare')
@@ -24,7 +40,8 @@ chromeWebStoreItemProperty(chromeStoreId).then(value => {
       }
     }
   )
-})
-  .catch(err => {
-    console.error('Failed getting version from Chrome Webstore', err)
-  })
+}
+
+start()
+  .then(console.log)
+  .catch(console.log)
