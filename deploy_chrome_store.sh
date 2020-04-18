9dd1a8fcd4782461bfb699ddb63e1c1e944dd9f9
Tal Bereznitskey <berzniz@gmail.com>
2020-04-18 20:43:03 +0300

    Update deplot scripts

--- deploy_chrome_store.sh
+++ deploy_chrome_store.sh
@@ -5,6 +5,8 @@ OUTPUT_TARGET=chrome
 OUTPUT_FOLDER=./build/$OUTPUT_TARGET/$ENV
 OUTPUT_FILE=$OUTPUT_TARGET.zip
 
+rm -f ./$OUTPUT_FILE
+
 yarn
 yarn test-ci
 NODE_ENV=production yarn build-$OUTPUT_TARGET
--- deploy_firefox_store.sh
+++ deploy_firefox_store.sh
@@ -5,9 +5,17 @@ OUTPUT_TARGET=firefox
 OUTPUT_FOLDER=./build/$OUTPUT_TARGET/$ENV
 OUTPUT_FILE=$OUTPUT_TARGET.zip
 
+rm -f ./$OUTPUT_FILE
+
 yarn
 yarn test-ci
 NODE_ENV=production yarn build-$OUTPUT_TARGET
 cd $OUTPUT_FOLDER
 zip -r ../../../$OUTPUT_FILE ./*
 cd -
+
+VERSION=`cat $OUTPUT_FOLDER/manifest.json | json version`
+zip -r firefox_source_$VERSION.zip src utils ./.babelrc package.json README.md webpack.config.js yarn.lock
+
+echo ""
+echo "Upload: https://addons.mozilla.org/en-US/developers/addons"
