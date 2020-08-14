2f802ce2e0a96771fc5fbe7533208b867bbf6746
Dave Myers <dmyers@squarespace.com>
2020-08-14 12:48:05 -0400

    Create xcode project for Safari 14 Web Extension Support

---
+++ safari-xcode-wrapper/Better Pull Request for GitHub/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Better Pull Request for GitHub
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+}
