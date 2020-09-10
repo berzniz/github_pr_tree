e4eb5a5b0041b4d4f7685dc0c3b3c12a7a4a724f
Idan Attias <idana@wix.com>
2020-09-10 12:47:15 +0300

    Mark directories as viewed when all their nested files/folders are viewed

--- src/js/components/file/index.jsx
+++ src/js/components/file/index.jsx
@@ -62,7 +62,7 @@ class File extends React.Component {
         ) : null}
         {isViewed ? (
           <svg className='github-pr-file-checkmark octicon octicon-check' viewBox='0 0 12 16' version='1.1' width='12' height='16' aria-hidden='true'>
-            <path fill-rule='evenodd' d='M12 5l-8 8-4-4 1.5-1.5L4 10l6.5-6.5L12 5z' />
+            <path fillRule='evenodd' d='M12 5l-8 8-4-4 1.5-1.5L4 10l6.5-6.5L12 5z' />
           </svg>
         ) : null}
       </div>
