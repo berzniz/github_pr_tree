419fc21a961252d4177153f074e43cb6e5b5a033
Antony Alkmim <antony.alkmim@stone.com.br>
2021-05-29 14:09:29 -0300

    feat: add script to export for safari

--- deploy_safari.sh
+++ deploy_safari.sh
@@ -14,7 +14,15 @@ NODE_ENV=production yarn build-$OUTPUT_TARGET
 # copy resources files into xcode project
 cp -r $OUTPUT_FOLDER/* Better\ Pull-Request/Better\ Pull-Request\ Extension/Resources
 
-cd $OUTPUT_FOLDER
-zip -r ../../../$OUTPUT_FILE ./*
-cd -
+# Build and archive safari extension
+
+cd ./Better\ Pull-Request/
+rm -rf ./build
+xcodebuild -project "Better Pull-Request.xcodeproj" -scheme "Better Pull-Request" archive -archivePath "build/Better Pull-Request.xcarchive"
+xcodebuild -exportArchive -archivePath "build/Better Pull-Request.xcarchive" -exportPath "Better Pull-Request.app" -exportOptionsPlist exportOptions.plist
+cd ..
+
+# cd $OUTPUT_FOLDER
+# zip -r ../../../$OUTPUT_FILE ./*
+# cd -
 
